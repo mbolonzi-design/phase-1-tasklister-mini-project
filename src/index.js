de1e92b33514fe14c7cd5d3bbc4e5314657a9467
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-15 09:37:05 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target.newTask.value)
+    form.reset()
+  })
 });
+
+function createToDo(newTodo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  li.textContent = `${newTodo}`
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#task').appendChild(li)
+  btn.addEventListener('click', (e) => e.target.parentNode.remove())
+}
